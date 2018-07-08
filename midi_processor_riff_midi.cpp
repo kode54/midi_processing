21a04c7cb2b40bd88a00e58dbdcc3551e868d61a
Vitaly Novichkov <admin@wohlnet.ru>
2018-07-08 08:44:55 +0300

    Little organize of riff_midi module

--- midi_processor_riff_midi.cpp
+++ midi_processor_riff_midi.cpp
@@ -2,15 +2,42 @@
 
 #include <string.h>
 
+static inline bool it_equal( std::vector<uint8_t>::const_iterator it1, const char *it2, size_t length )
+{
+    for ( size_t i = 0; i < length; i++ )
+    {
+        if ( it1[ i ] != it2[ i ] )
+            return false;
+    }
+    return true;
+}
+
+static inline uint32_t toInt32LE( const uint8_t *in )
+{
+    return static_cast<uint32_t>( in[ 0 ] ) |
+           static_cast<uint32_t>( in[ 1 ] << 8 ) |
+           static_cast<uint32_t>( in[ 2 ] << 16 ) |
+           static_cast<uint32_t>( in[ 3 ] << 24 );
+}
+
+static inline uint32_t toInt32LE( std::vector<uint8_t>::const_iterator in )
+{
+    return static_cast<uint32_t>( in[ 0 ] ) |
+           static_cast<uint32_t>( in[ 1 ] << 8 ) |
+           static_cast<uint32_t>( in[ 2 ] << 16 ) |
+           static_cast<uint32_t>( in[ 3 ] << 24 );
+}
+
+
 bool midi_processor::is_riff_midi( std::vector<uint8_t> const& p_file )
 {
     if ( p_file.size() < 20 ) return false;
-    if ( p_file[ 0 ] != 'R' || p_file[ 1 ] != 'I' || p_file[ 2 ] != 'F' || p_file[ 3 ] != 'F' ) return false;
+    if ( memcmp( &p_file[ 0 ], "RIFF", 4 ) != 0 ) return false;
     uint32_t riff_size = p_file[ 4 ] | ( p_file[ 5 ] << 8 ) | ( p_file[ 6 ] << 16 ) | ( p_file[ 7 ] << 24 );
     if ( riff_size < 12 || ( p_file.size() < riff_size + 8 ) ) return false;
-    if ( p_file[ 8 ] != 'R' || p_file[ 9 ] != 'M' || p_file[ 10 ] != 'I' || p_file[ 11 ] != 'D' ||
-         p_file[ 12 ] != 'd' || p_file[ 13 ] != 'a' || p_file[ 14 ] != 't' || p_file[ 15 ] != 'a' ) return false;
-    uint32_t data_size = p_file[ 16 ] | ( p_file[ 17 ] << 8 ) | ( p_file[ 18 ] << 16 ) | ( p_file[ 19 ] << 24 );
+    if ( memcmp( &p_file[ 8 ], "RMID", 4 ) != 0 ||
+         memcmp( &p_file[ 12 ], "data", 4 ) != 0 ) return false;
+    uint32_t data_size = toInt32LE(&p_file[ 16 ]);
     if ( data_size < 18 || p_file.size() < data_size + 20 || riff_size < data_size + 12 ) return false;
     std::vector<uint8_t> test;
     test.assign( p_file.begin() + 20, p_file.begin() + 20 + 18 );
@@ -34,7 +61,7 @@ bool midi_processor::process_riff_midi_count( std::vector<uint8_t> const& p_file
         if ( body_end - it < 8 ) return false;
         uint32_t chunk_size = it[ 4 ] | ( it[ 5 ] << 8 ) | ( it[ 6 ] << 16 ) | ( it[ 7 ] << 24 );
         if ( (unsigned long)(body_end - it) < chunk_size ) return false;
-        if ( it[ 0 ] == 'd' && it[ 1 ] == 'a' && it[ 2 ] == 't' && it[ 3 ] == 'a' )
+        if ( it_equal(it, "data", 4) )
         {
             std::vector<uint8_t> midi_file;
             midi_file.assign( it + 8, it + 8 + chunk_size );
@@ -92,9 +119,9 @@ bool midi_processor::process_riff_midi( std::vector<uint8_t> const& p_file, midi
     while ( it != body_end )
     {
         if ( body_end - it < 8 ) return false;
-        uint32_t chunk_size = it[ 4 ] | ( it[ 5 ] << 8 ) | ( it[ 6 ] << 16 ) | ( it[ 7 ] << 24 );
+        uint32_t chunk_size = toInt32LE( it + 4 );
         if ( (unsigned long)(body_end - it) < chunk_size ) return false;
-        if ( it[ 0 ] == 'd' && it[ 1 ] == 'a' && it[ 2 ] == 't' && it[ 3 ] == 'a' )
+        if ( it_equal( it, "data", 4 ) )
         {
             if ( !found_data )
             {
@@ -107,9 +134,9 @@ bool midi_processor::process_riff_midi( std::vector<uint8_t> const& p_file, midi
             it += 8 + chunk_size;
             if ( chunk_size & 1 && it != body_end ) ++it;
         }
-        else if ( it[ 0 ] == 'D' && it[ 1 ] == 'I' && it[ 2 ] == 'S' && it[ 3 ] == 'P' )
+        else if ( it_equal( it, "DISP", 4 ) )
         {
-            uint32_t type = it[ 8 ] | ( it[ 9 ] << 8 ) | ( it[ 10 ] << 16 ) | ( it[ 11 ] << 24 );
+            uint32_t type = toInt32LE( it + 8 );
             if ( type == 1 )
             {
                 extra_buffer.resize( chunk_size - 4 );
@@ -119,10 +146,10 @@ bool midi_processor::process_riff_midi( std::vector<uint8_t> const& p_file, midi
             it += 8 + chunk_size;
             if ( chunk_size & 1 && it != body_end ) ++it;
         }
-        else if ( it[ 0 ] == 'L' && it[ 1 ] == 'I' && it[ 2 ] == 'S' && it[ 3 ] == 'T' )
+        else if ( it_equal( it, "LIST", 4 ) )
         {
             std::vector<uint8_t>::const_iterator chunk_end = it + 8 + chunk_size;
-            if ( it[ 8 ] == 'I' && it[ 9 ] == 'N' && it[ 10 ] == 'F' && it[ 11 ] == 'O' )
+            if ( it_equal( it + 8, "INFO", 4 ) )
             {
                 if ( !found_info )
                 {
@@ -131,7 +158,7 @@ bool midi_processor::process_riff_midi( std::vector<uint8_t> const& p_file, midi
                     while ( it != chunk_end )
                     {
                         if ( chunk_end - it < 4 ) return false;
-                        uint32_t field_size = it[ 4 ] | ( it[ 5 ] << 8 ) | ( it[ 6 ] << 16 ) | ( it[ 7 ] << 24 );
+                        uint32_t field_size = toInt32LE( it + 4 );
                         if ( (unsigned long)(chunk_end - it) < 8 + field_size ) return false;
                         std::string field;
                         field.assign( it, it + 4 );
